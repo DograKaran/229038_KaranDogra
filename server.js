ee4caf6a42a5e741591125649ca214ec95a98dad
dac <dac@dypm.org>
2023-01-02 17:04:29 +0530

    updates

--- server.js
+++ server.js
@@ -1,43 +1,43 @@
-const express=require('express');
-const app=express();
+const express = require('express');
+const app = express();
 
-app.get('/',(req,res)=>{
+app.get('/',(req, res) => {
     res.send(
         "<h1>SANGAM ELECTRONICS</h1>"
-        +"<hr/>"
-        +"<h3>World of Computer Electronics Store</h3>"
-        +"<br/>"
-        +"<ol>"
-        +"<li>Mobiles</li>"
-        +"<li>PC</li>"
-        +"<li>Laptops</li>"
-        +"<li>Hard Disk</li>"
-        +"<li>Gaming Laptops</li>"
-        +"<li>other Boards</li>"
-        +"</ol>"
+        + "<hr/>"
+        + "<h3>World of Computer Electronics Store</h3>"
+        + "<br/>"
+        + "<ol>"
+        + "<li>Mobiles</li>"
+        + "<li>PC</li>"
+        + "<li>Laptops</li>"
+        + "<li>Hard Disk</li>"
+        + "<li>Gaming Laptops</li>"
+        + "<li>other Boards</li>"
+        + "</ol>"            
+
+    )
+});
+
+
+app.get('/about', (req, res) => {
+    res.send(
+        "<h1>SANGAM ELECTRONICS</h1>"
+        + "<hr/>"
+        + "<h3>World of Computer Electronics Store</h3>"
+        + "<br/>"
+        + "<ol>"
+        + "<li>Mobiles</li>"
+        + "<li>PC</li>"
+        + "<li>Laptops</li>"
+        + "<li>Hard Disk</li>"
+        + "<li>Gaming Laptops</li>"
+        + "<li>other Boards</li>"
+        + "</ol>"
 
     );
 });
 
 
-app.get('/about',(req,res)=>{
-    res.send(
-        "<h1>SANGAM ELECTRONICS</h1>"
-        +"<hr/>"
-        +"<h3>World of Computer Electronics Store</h3>"
-        +"<br/>"
-        +"<ol>"
-        +"<li>Mobiles</li>"
-        +"<li>PC</li>"
-        +"<li>Laptops</li>"
-        +"<li>Hard Disk</li>"
-        +"<li>Gaming Laptops</li>"
-        +"<li>other Boards</li>"
-        +"</ol>"
-
-    );
-});
-
-
-var server=app.connect(9000);
+var server = app.listen(9000);
 console.log("Server Has Started on Port No:9000");
